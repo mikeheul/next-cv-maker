b0c2f93cba0893ecefcf952c641189c5bc2e2504
mikeheul <mickael.murmann@gmail.com>
2025-02-05 16:55:23 +0100

    Refactor fetchCandidate and fetchCandidates hooks to remove commented-out code and streamline base URL assignment

--- app/api/candidate/hooks/fetchCandidate.ts
+++ app/api/candidate/hooks/fetchCandidate.ts
@@ -1,8 +1,6 @@
 import { Candidate } from "@/types/types";
 
-// one candidate
 export const fetchCandidate = async (candidateId: string): Promise<Candidate> => {
-    // const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/candidate/${candidateId}`, {
     const baseUrl = process.env.NODE_ENV === 'production'
         ? 'https://next-cv-maker.vercel.app' 
         : 'http://localhost:3000'; 
--- app/api/candidate/hooks/fetchCandidates.ts
+++ app/api/candidate/hooks/fetchCandidates.ts
@@ -1,7 +1,5 @@
-// all candidates 
 export const fetchCandidates = async () => {
-    // const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/candidate`, {
-        const baseUrl = process.env.NODE_ENV === 'production'
+    const baseUrl = process.env.NODE_ENV === 'production'
         ? 'https://next-cv-maker.vercel.app' 
         : 'http://localhost:3000'; 
 
