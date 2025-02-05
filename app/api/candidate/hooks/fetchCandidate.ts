572d1a1399058ab88761afe49bdd6426e74b4173
mikeheul <mickael.murmann@gmail.com>
2025-02-05 16:45:17 +0100

    Update fetchCandidate and fetchCandidates hooks to use environment-specific base URLs

--- app/api/candidate/hooks/fetchCandidate.ts
+++ app/api/candidate/hooks/fetchCandidate.ts
@@ -2,10 +2,18 @@ import { Candidate } from "@/types/types";
 
 // one candidate
 export const fetchCandidate = async (candidateId: string): Promise<Candidate> => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/candidate/${candidateId}`, {
-        next: { revalidate: 60 }, 
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/candidate/${candidateId}`, {
+    const baseUrl = process.env.NODE_ENV === 'production'
+        ? 'https://next-cv-maker.vercel.app' 
+        : 'http://localhost:3000'; 
+
+    const res = await fetch(`${baseUrl}/api/candidate/${candidateId}`, {
+        next: { revalidate: 60 },
         cache: "no-store",
     });
     if (!res.ok) throw new Error("Failed to fetch candidate");
     return res.json();
 };
+
+
+
--- app/api/candidate/hooks/fetchCandidates.ts
+++ app/api/candidate/hooks/fetchCandidates.ts
@@ -1,7 +1,13 @@
 // all candidates 
 export const fetchCandidates = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/candidate`, {
-        next: { revalidate: 60 }, 
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/candidate`, {
+        const baseUrl = process.env.NODE_ENV === 'production'
+        ? 'https://next-cv-maker.vercel.app' 
+        : 'http://localhost:3000'; 
+
+    const res = await fetch(`${baseUrl}/api/candidate/`, {
+        next: { revalidate: 60 },
+        cache: "no-store",
     });
     if (!res.ok) throw new Error("Failed to fetch candidates");
     return res.json();
