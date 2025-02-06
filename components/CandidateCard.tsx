0e279a8ffd9ce990954674633885e515a9dc13d6
mikeheul <mickael.murmann@gmail.com>
2025-02-06 11:36:51 +0100

    Refactor CandidateCard component by removing commented-out code for cleaner implementation

--- components/CandidateCard.tsx
+++ components/CandidateCard.tsx
@@ -6,14 +6,6 @@ import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 function CandidateCard({ candidate }: CandidateCardProps) {
 
     return (
-        // <Link href={`/candidate/${candidate.id}`} passHref>
-        //     <div className="p-6 rounded-lg shadow-2xl bg-gray-700 text-white cursor-pointer">
-        //         <h2 className="text-xl font-bold">{ candidate.firstName } { candidate.lastName }</h2>
-        //         <p className="text-gray-300">Email : { candidate.email }</p>
-        //         { candidate.phone && <p className="text-gray-300">Téléphone : { candidate.phone }</p>}
-        //         { candidate.address && <p className="text-gray-300">Adresse : { candidate.address }</p>}
-        //     </div>
-        // </Link>
         <Link href={`/candidate/${candidate.id}`} passHref>
             <Card className="cursor-pointer bg-gray-700 text-white hover:shadow-lg transition-all">
                 <CardHeader>
