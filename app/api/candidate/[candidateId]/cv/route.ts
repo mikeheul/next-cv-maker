e5f0a5153148c9d2509964004ddaaf8b86afb30e
mikeheul <mickael.murmann@gmail.com>
2025-02-06 09:29:54 +0100

    Add PDF download functionality for candidate CV and update package dependencies

---
+++ app/api/candidate/[candidateId]/cv/route.ts
@@ -0,0 +1,30 @@
+import { db } from '@/lib/db';
+import { NextResponse } from 'next/server';
+
+type Props = {
+    params: Promise<{ candidateId: string }>
+}
+
+export async function GET(req: Request, { params }: Props) {
+    const { candidateId } = await params;
+
+    try {
+        // Récupérer les données du candidat depuis la base
+        const candidate = await db.candidate.findUnique({
+            where: { id: candidateId },
+            include: {
+                experiences: { orderBy: { startDate: 'desc' } }
+            }
+        });
+
+        if (!candidate) {
+            return NextResponse.json({ message: 'Candidat non trouvé' }, { status: 404 });
+        }
+
+        return NextResponse.json(candidate);
+
+    } catch (error) {
+        console.error('Erreur lors de la génération du CV :', error);
+        return NextResponse.json({ message: 'Erreur interne' }, { status: 500 });
+    }
+}
--- app/candidate/[candidateId]/page.tsx
+++ app/candidate/[candidateId]/page.tsx
@@ -18,13 +18,23 @@ const CandidatePage = async ({ params }: PageProps) => {
         <>
             <h1 className="py-4 text-4xl font-bold">Candidat</h1>
 
-            <Link 
-                href="/candidate"
-                className="inline-flex items-center text-sm text-white bg-gray-700 hover:bg-gray-600 py-2 px-4 rounded-md mb-6 transition-colors duration-300"
-            >
-                <ArrowLeftIcon className="w-5 h-5 mr-2" /> Retour à la liste des candidats
-            </Link>
-    
+            <div className="flex flex-col py-6">
+                <Link 
+                    href="/candidate"
+                    className="inline-flex items-center text-sm text-white bg-gray-700 hover:bg-gray-600 py-2 px-4 rounded-md mb-6 transition-colors duration-300 m-auto"
+                >
+                    <ArrowLeftIcon className="w-5 h-5 mr-2" /> Retour à la liste des candidats
+                </Link>
+
+                <Link 
+                    href={`/api/candidate/${candidateId}/cv`} 
+                    className="inline-block bg-blue-600 text-white px-4 py-2 rounded-md m-auto"
+                    target="_blank"
+                >
+                    Télécharger le CV en PDF
+                </Link>
+            </div>
+
             <div className="border-l pl-4">
                 <h2 className="text-2xl ">{ candidate.firstName } { candidate.lastName }</h2>
                 <p className="text-gray-300">Email : { candidate.email }</p>
