b1c5c22dbef76cc542fdb4f0552de46e7d2493a0
mikeheul <mickael.murmann@gmail.com>
2025-02-07 09:50:15 +0100

    Remove search form from CandidatesPage for streamlined UI

--- app/candidate/page.tsx
+++ app/candidate/page.tsx
@@ -15,20 +15,6 @@ const CandidatesPage = async ({ searchParams }: CandidatesPageProps) => {
         <>
             <h1 className="py-4 text-4xl text-white font-bold">Candidats</h1>
 
-            {/* Ajout du champ de recherche */}
-            <form action="/candidate" method="GET" className="mb-4">
-                <input
-                    type="text"
-                    name="search"
-                    placeholder="Rechercher par nom..."
-                    defaultValue={search}
-                    className="p-2 border rounded-md w-full bg-gray-800 text-white"
-                />
-                <button type="submit" className="mt-2 p-2 bg-blue-600 text-white rounded-md">
-                    Filtrer
-                </button>
-            </form>
-
             <CandidateList
                 candidates={candidates}
             />
